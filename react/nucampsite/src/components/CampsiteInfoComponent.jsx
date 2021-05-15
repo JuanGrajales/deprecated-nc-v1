57388c71d9b239489744aabe9f1c3e47f64e1c34
JuanGrajales <inbox1.juan+github@gmail.com>
2021-05-15 12:46:22 -0400

    feat(react-w2): add task 2

--- react/nucampsite/src/components/CampsiteInfoComponent.jsx
+++ react/nucampsite/src/components/CampsiteInfoComponent.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class CampsiteInfo extends Component {
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
   render() {
     // only display the data of the campsite if the data exists (i.e. when the user clicks on a campsite)
     if (this.props.campsite) {
-      return <div className="row">campsite info when it has been selected</div>;
+      return (
+        <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+      );
     }
     // if the user hasn't clicked on a campsite then display nothing
     return <div>no campsite selected yet</div>;

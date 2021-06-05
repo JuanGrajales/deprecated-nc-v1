e169ebaea85d62c30d671b69dfe0e5349ce89efe
JuanGrajales <inbox1.juan+github@gmail.com>
2021-06-05 13:04:45 -0400

    feat(react-w5): add task 2 and 3

--- react/nucampsite/src/components/MainComponent.js
+++ react/nucampsite/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { connect } from "react-redux";
 import { actions } from "react-redux-form";
 import {
   postComment,
+  postFeedback,
   fetchCampsites,
   fetchComments,
   fetchPromotions,
@@ -35,6 +36,7 @@ const mapDispatchToProps = {
   fetchComments: () => fetchComments(),
   fetchPromotions: () => fetchPromotions(),
   fetchPartners: () => fetchPartners(),
+  postFeedback: (feedback) => postFeedback(feedback),
 };
 
 class Main extends Component {
@@ -114,7 +116,10 @@ class Main extends Component {
                 exact
                 path="/contactus"
                 render={() => (
-                  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                  <Contact
+                    resetFeedbackForm={this.props.resetFeedbackForm}
+                    postFeedback={this.props.postFeedback}
+                  />
                 )}
               />
               <Route

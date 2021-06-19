d1ff024898fb7d623a902b20f52c882c5ec6619e
JuanGrajales <inbox1.juan+github@gmail.com>
2021-06-19 12:35:24 -0400

    feat(react-w2-workshops): add task 3

--- react/w2/workshop/nucampsite/src/components/CampsiteInfoComponent.js
+++ react/w2/workshop/nucampsite/src/components/CampsiteInfoComponent.js
@@ -16,10 +16,39 @@ class CampsiteInfo extends Component {
     );
   }
 
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => {
+            return (
+              <div>
+                <p>
+                  {comment.text}
+                  <br />
+                  --{comment.author}{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  }
+
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
       );
     } else {
       return <div>No campsite selected</div>;

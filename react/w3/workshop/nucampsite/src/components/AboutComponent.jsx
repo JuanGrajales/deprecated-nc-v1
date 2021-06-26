bbbb20d78cca0733937f14a07a20596e2800cce4
Juan Grajales <inbox1.juan+github@gmail.com>
2021-06-26 11:05:23 -0400

    feat: add about component

--- react/w3/workshop/nucampsite/src/components/AboutComponent.jsx
+++ react/w3/workshop/nucampsite/src/components/AboutComponent.jsx
@@ -9,28 +9,9 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
-function RenderPartner({ partner }) {
-  if (partner) {
-    return (
-      <React.Fragment>
-        <Media object src={partner.image} alt={partner.name} width="150" />
-        <Media body className="ml-5 mb-4">
-          <Media heading>{partner.name}</Media>
-          {partner.description}
-        </Media>
-      </React.Fragment>
-    );
-  }
-  return <div />;
-}
-
 function About(props) {
   const partners = props.partners.map((partner) => {
-    return (
-      <Media tag="li" key={partner.id}>
-        <RenderPartner partner={partner} />
-      </Media>
-    );
+    return <h5>{partner.name}</h5>;
   });
 
   return (

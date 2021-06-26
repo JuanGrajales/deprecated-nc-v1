31735c25d339785ace72d7c18cf681abebe35493
Juan Grajales <inbox1.juan+github@gmail.com>
2021-06-25 21:44:36 -0400

    feat(react-w3-lectures): add w3 lectures
    fix(react-w3-lectures): update w3 lectures

---
+++ react/w3/lectures/2-functional-components/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    p1: "juan",
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.p1}</h1>
+        <button onClick={() => this.setState({ p1: "Rod" })}>
+          Change Name
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/w3/lectures/4-react-fragment/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Boycott React Fragment</div>
+        <div>And here too!</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;

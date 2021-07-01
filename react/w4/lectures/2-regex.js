a43191a2b2a7c5c05ab20906410a41831c80b360
Juan Grajales <inbox1.juan+github@gmail.com>
2021-06-30 21:14:09 -0400

    feat(react-w4-lectures): add w4 lectures

---
+++ react/w4/lectures/2-regex.js
@@ -0,0 +1,31 @@
+// sometimes you want to find parts (i.e. substrings) of a string
+
+// example
+let str = "bye";
+
+// console.log(str.replace("b", "a")); // this prints "aye"
+// but what if you have multiple 'b's not just one?
+// this is where regex could come in handy
+
+let str1 = "bbbbbbbye";
+// console.log(str1.replace("b", "a")); // this prints "abbbbbbye"
+
+// regex syntax: /pattern/optionalFlags
+
+// basic example
+let regex = /b/g;
+let str2 = "bbbbbbbye";
+// console.log(str2.replace(regex, "a")); // this still prints "aaaaaaaye"
+
+// global flag example
+let regex2 = /[ebyc]/gi;
+let str3 = "BBbBbBbye";
+// console.log(str3.replace(regex2, "a")); // this still prints "aaaaaaaaa"
+
+let regex3 = /^\w{4,6}/;
+let str4 = "1234567";
+// console.log(regex3.test(str4));
+
+// resources
+// https://www.w3schools.com/js/js_regexp.asp
+// https://www.rexegg.com/regex-quickstart.html
